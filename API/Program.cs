887e8de49d325ad3980606808e3497263b7548c1
@programmingst <lucky.a.acidera@gmail.com>
2026-01-02 23:59:16 +0800

    SETUP WEBAPP CLIENT WITH SSL

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,18 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200");
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("CorsPolicy");
 app.MapControllers();
 
 app.Run();
---
+++ client/src/app/app.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App implements OnInit {
+  private http = inject(HttpClient);
+  protected readonly title = signal('client');
+  protected members = signal<any>([]);
+
+  async ngOnInit(){
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
+      console.error('Error fetching members data:', error);
+      throw error;
+    }
+  }
+}
